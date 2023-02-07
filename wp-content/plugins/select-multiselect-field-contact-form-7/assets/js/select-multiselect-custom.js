1b24ad20027d412796dc523e4b6e0e5fefafa935
kaleriapog <pogrelova.kaleria@gmail.com>
2023-02-07 17:55:14 +0200

    fix bags

--- wp-content/plugins/select-multiselect-field-contact-form-7/assets/js/select-multiselect-custom.js
+++
@@ -1,9 +0,0 @@
-/*Select and Multi-Select Custom jQuery Events*/
-jQuery('.wpcf7').ready(function(){
-    jQuery('select.wpcf7-selct-multiselct').select2();
-    jQuery('select.wpcf7-selct-multiselct').val(null).trigger('change');
-});
-
-jQuery(".wpcf7").on( 'wpcf7:mailsent', function( event ){
-    jQuery('select.wpcf7-selct-multiselct').val(null).trigger('change');
-});
--- wp-content/plugins/select-multiselect-field-contact-form-7/includes/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*Including Files*/
-
-if( class_exists( 'WPCF7' ) ){
-    require YB_SM_PATH. 'includes/form-tag.php';
-    require YB_SM_PATH. 'includes/filter-tag.php';
-    require YB_SM_PATH. 'includes/tag.php';
-}
-?>
