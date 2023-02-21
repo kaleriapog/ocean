eedc25cc61a021781ad01322d7f728b0652f5a2e
kaleriapog <pogrelova.kaleria@gmail.com>
2023-02-21 11:08:16 +0200

    add autoscroll in slider in hero

--- wp-content/themes/ocean/js/scripts.js
+++ wp-content/themes/ocean/js/scripts.js
@@ -66,11 +66,41 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     if(sliderAchievements) {
-        new Swiper('.swiper-achievements', {
+        let sliderAchievementsSwiper = new Swiper('.swiper-achievements', {
             direction: 'horizontal',
             slidesPerView: 'auto',
             spaceBetween: 100,
+            allowTouchMove: false,
+            speed: 10000,
+            autoplay: {
+                delay: 1,
+            },
         });
+
+        sliderAchievementsSwiper.autoplay.stop()
+
+
+        new ScrollMagic.Scene({triggerElement: '.section-slider', duration: '100%', triggerHook: 'onLeave' })
+            .addIndicators({name: 'section-slider'})
+            .addTo(controller)
+            .on('start end', (e) => {
+                if(e.state === 'DURING' && e.type === 'start') {
+                    sliderAchievementsSwiper.autoplay.start()
+                }
+
+                if(e.state === 'BEFORE' && e.type === 'start') {
+                    sliderAchievementsSwiper.autoplay.stop()
+                }
+
+                if(e.state === 'AFTER' && e.type === 'end') {
+                    sliderAchievementsSwiper.autoplay.stop()
+                }
+
+                if(e.state === 'DURING' && e.type === 'end') {
+                    sliderAchievementsSwiper.autoplay.start()
+                }
+            })
+            .reverse(true);
     }
 
     if(verticalSliders) {
